7bc1193f005de8a1a87b6233ff3c3418803f0cf2
cvilla <villac@mit.edu>
2025-04-16 15:48:37 -0400

    workload isn't working, tried altering constraints based on paper

--- workspace/models/arch/1_macro/scc_proj_2025/_tests.py
+++ workspace/models/arch/1_macro/scc_proj_2025/_tests.py
@@ -1,11 +1,14 @@
 import sys
 import os
+import yaml
+import copy
 
 # fmt: off
 THIS_SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 MACRO_NAME = os.path.basename(THIS_SCRIPT_DIR)
 sys.path.append(os.path.abspath(os.path.join(THIS_SCRIPT_DIR, '..', '..', '..', '..')))
 from scripts import utils as utl
+import helper_functions
 import scripts
 # fmt: on
 
@@ -29,6 +32,35 @@ def test_area_energy_breakdown():
     results.clear_zero_areas()
     results.clear_zero_energies()
     return results
+
+def test_mm_energy_breakdown():
+    """
+    ### Matrix Multiply Energy Breakdown 
+
+    This test evaluates the energy breakdown for a matrix multiply workload
+    where both input matrix A and weight matrix B are drawn from a Gaussian
+    distribution with inputted sparsity.
+    
+    Possible Parameters for future:
+        bitwdith (int) (optional): size of input bits, if you're using INT4/INT4-VSQ or INT8
+        sparsity (float) (optional): decimal sparsity amount, 0.5 == 50% sparsity
+        std (float) (optional): standard deviationi for Gaussian distribution
+    """
+
+    results = utl.run_layer(
+        macro=MACRO_NAME,
+        layer="simple_matmul",
+        variables=dict(
+            INPUT_BITS=4,
+            WEIGHT_BITS=4,
+            OUTPUT_BITS=4,
+        ),
+    )
+
+    results.clear_zero_areas()
+    results.clear_zero_energies()
+    return results
+
 
 if __name__ == "__main__":
     test_area_energy_breakdown()
